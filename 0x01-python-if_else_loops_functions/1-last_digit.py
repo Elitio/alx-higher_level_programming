0ce586781cf277c91d4e55e0dd02b4c258200778
Christian Annan Segbenu <elitio33@gmail.com>
2023-08-08 06:48:59 -0700

    Print message according to number printed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-if last_digit < 0:
-    abs(last_digit)
+l_int = number % 10
 
-if last_digit > 5:
-    print(f"Last digit of {number:d} is {last_digit} and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number:d} is {last_digit} and is 0")
-elif last_digit > 0 and last_digit < 6:
-    print(f"Last digit of {number:d} is {last_digit} and is less than 6 and not 0")
+if l_int > 5:
+    print(f"Last digit of {number:d} is {l_int} and is greater than 5")
+elif l_int == 0:
+    print(f"Last digit of {number:d} is {l_int} and is 0")
+elif last_int > 0 and last_digit < 6:
+    print(f"Last digit of {number:d} is {l_int} and is less than 6 and not 0")
