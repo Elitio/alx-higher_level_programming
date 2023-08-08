42efdf4d6e85a3595c9889225e339b00b3cbe93a
Christian Annan Segbenu <elitio33@gmail.com>
2023-08-08 06:51:54 -0700

    Print message according to number printed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,5 @@ if l_int > 5:
     print(f"Last digit of {number:d} is {l_int} and is greater than 5")
 elif l_int == 0:
     print(f"Last digit of {number:d} is {l_int} and is 0")
-elif last_int > 0 and last_digit < 6:
+elif last_digit < 6:
     print(f"Last digit of {number:d} is {l_int} and is less than 6 and not 0")
