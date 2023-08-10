576e2f38c98381d3382c8c722b0f95114618bf94
Christian Annan Segbenu <elitio33@gmail.com>
2023-08-10 15:36:50 -0700

    A program that imports functions

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -7,5 +7,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    answer = add(a, b)
-    print("{} + {} = {}".format(a, b, answer))
+    print("{} + {} = {}".format(a, b, (a + B)))
