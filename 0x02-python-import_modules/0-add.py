f2346a7abee8002320d7d3b9adae558f15effe9e
Christian Annan Segbenu <elitio33@gmail.com>
2023-08-10 15:03:37 -0700

    A program that imports functions

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A program that  imports the function def add(a, b): from the file a
+prints the result of the addition 1 + 2 = 3
+"""
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    answer = add(a, b)
+    print("{} + {} = {}".format(a, b, answer)
