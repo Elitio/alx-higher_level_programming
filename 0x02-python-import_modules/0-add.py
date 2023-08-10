73e47f7acfcfb4388f71262615a433d973041383
Christian Annan Segbenu <elitio33@gmail.com>
2023-08-10 15:30:35 -0700

    A program that imports functions

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
 A program that  imports the function def add(a, b): from the file a
-prints the result of the addition 1 + 2 = 3
+prints the answer of the addition 1 + 2 = 3
 """
 if __name__ == "__main__":
+    main()
     from add_0 import add
     a = 1
     b = 2
