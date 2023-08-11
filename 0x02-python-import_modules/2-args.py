a152f0845305fbd6533c972a5adabda2c585b534
Christian Annan Segbenu <elitio33@gmail.com>
2023-08-11 16:08:15 -0700

    Write a program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,19 @@
 #!/bin/python3
-"""
-A program that displays the number of and the list of its arguments
-"""
-
 if __name__ = "__main__":
     from sys import argv
-    count = len(argv)
+    count = len(argv) - 1
     # When no arguments are passed
-    if count == 1:
-        print("{} arguments.".format(count - 1))
+    if count == 0:
+        print("{} arguments.".format(count))
     # when 1 argument is passed
-    if counter == 2:
-        print("{} argument:".format(count - 1))
+    if counter == 1:
+        print("{} argument:".format(count))
     # When more arguments
     else:
-        print("{} arguments:".format(count - 1))
+        print("{} arguments:".format(count))
     # This will display the number of args
-    for i in range(1, counter):
-        print("{}: {}".format(i, argv[i]))
+    if count >= 1:
+        count = 0
+        for arg in argv:
+            if != 0:
+                print("{}: {}".format(count, arg)
