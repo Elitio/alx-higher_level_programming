ce7b6e296ed26116a9a821a8d1a004ae3b7cca35
Christian Annan Segbenu <elitio33@gmail.com>
2023-08-11 17:52:42 -0700

    Write a program that prints all the names defined by the compiled module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
-    from sys import argv
-    count = len(argv) - 1
+    import sys
+    count = len(sys.argv) - 1
     # When no arguments are passed
     if count == 0:
         print("{} arguments.".format(count))
     # when 1 argument is passed
-    if counter == 1:
+    elif counter == 1:
         print("{} argument:".format(count))
     # When more arguments
     else:
@@ -14,6 +14,7 @@ if __name__ = "__main__":
     # This will display the number of args
     if count >= 1:
         count = 0
-        for arg in argv:
+        for arg in sys.argv:
             if != 0:
                 print("{}: {}".format(count, arg))
+            count += 1
