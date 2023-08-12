f9ecfef1ec08b44bb5ce8f79ed6746b67231f850
Christian Annan Segbenu <elitio33@gmail.com>
2023-08-11 17:15:30 -0700

    Write a program that prints all the names defined by the compiled module

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,6 +5,7 @@ A program thats displays the sum of all argv[]
 if __name == "__main__":
     from sys import argv
     sum = 0
-    for i in argv[1:]:
-        sum += int(i)
-        print("{}".format(sum))
+    for i in argv:
+        if i != argv[0]:
+            sum += int(i)
+    print("{}".format(sum))
