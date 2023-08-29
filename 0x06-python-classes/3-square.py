6a0ced241e8431a1c32a3b471e3543ee444b989d
Christian Annan Segbenu <elitio33@gmail.com>
2023-08-29 16:43:15 -0700

    A class Square that defines a square by private instance attribute: size and write a public inctance area that compute the square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
+"""
+Write a class Square that defines a square by: (based on 2-square.py)
+
+Private instance attribute: size
+"""
+
+
 class Square:
     """ A class that defines a square by its size
     """
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
-        if not isinstance(size, int):
+        """Instantiation with optional size=0"""
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
-        """ Method that returns the square of theobject"""
-
-        return (self.__size ** 2)
+        """"Square area and returns the area"""
+        return self.__size ** 2
