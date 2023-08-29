dc0c4fb59d139c9740c0eac2706b6958700072c5
Christian Annan Segbenu <elitio33@gmail.com>
2023-08-29 16:21:59 -0700

    A class Square that defines a square by private instance attribute: size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,15 +10,11 @@ class Square:
     to 0 """
 
     def __init__(self, size=0):
-
         """ raiseing a TypeError if size is not an integer"""
+
         if not isinstance(size, int):
-
             raise TypeError("size must be integer")
-        """ Now raise a ValueError when size is less than 0"""
         elif size < 0:
-
-            """ Raise ValueError with message"""
             raise ValueError("size must be >= 0")
         else:
             self.__size = int(size)
